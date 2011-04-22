045e8c2e9bf0cc52d07eda218c5fbd4b5bcdf1df
nashby <younash@gmail.com>
2011-04-22 20:49:48 +0300

    Regenerate gemspec for version 0.2.0

--- lib/cityhash/version.rb
+++ lib/cityhash/version.rb
@@ -2,8 +2,8 @@ module CityHash
  
   module Version
     MAJOR = 0
-    MINOR = 1
-    PATCH = 1
+    MINOR = 2
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
