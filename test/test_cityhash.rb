496dd3d8aefe9599e2843b256e460078ddc80bc7
Johannes Holzfuß <DataWraith@web.de>
2011-04-22 19:14:26 +0200

    Update Tests to use the ruby-wrapper

--- test/test_cityhash.rb
+++ test/test_cityhash.rb
@@ -1,16 +1,17 @@
 require 'helper'
 
 class TestCityhash < Test::Unit::TestCase
+
   should "return 64bit hash" do
-    assert_equal 17703940110308125106, CityHash.city_hash64("test", 4)
+    assert_equal 17703940110308125106, CityHash.hash64("test")
   end
-  
+
   should "return 64bit hash with a seed" do
-    assert_equal 14900027982776226655, CityHash.city_hash64_with_seed("test", 4, 12345)
-  end  
-  
+    assert_equal 14900027982776226655, CityHash.hash64("test", 12345)
+  end
+
   should "return 64bit hash with seeds" do
-    assert_equal 11136353178704814373, CityHash.city_hash64_with_seeds("test", 4, 12345, 54321)
-  end   
-  
+    assert_equal 11136353178704814373, CityHash.hash64("test", 12345, 54321)
+  end
+
 end
