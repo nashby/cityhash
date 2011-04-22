a68a2e42197761482a78ac3ac351236036dbbcc1
nashby <younash@gmail.com>
2011-04-22 18:49:32 +0300

    path fix

--- lib/cityhash.rb
+++ lib/cityhash.rb
@@ -3,7 +3,7 @@ require 'ffi'
 module CityHash
   extend FFI::Library
   
-  ffi_lib "lib/cityhash/ext/libcity.so"
+  ffi_lib File.dirname(__FILE__)+"/cityhash/ext/libcity.so"
   attach_function :city_hash64, :CityHash64, [:string, :size_t], :uint64
   attach_function :city_hash64_with_seed, :CityHash64WithSeed, [:string, :size_t, :uint64], :uint64
   attach_function :city_hash64_with_seeds, :CityHash64WithSeeds, [:string, :size_t, :uint64, :uint64], :uint64
