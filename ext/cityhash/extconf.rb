409a1d704a1000a7f14d7f9b99f02fd3b2358c44
Quin Hoxie <qhoxie@gmail.com>
2011-10-14 11:03:17 -0700

    tighten up cppflags

--- ext/cityhash/extconf.rb
+++ ext/cityhash/extconf.rb
@@ -2,9 +2,10 @@ require 'mkmf'
 
 have_library('objc') if RUBY_PLATFORM =~ /darwin/
 
-$CPPFLAGS += " -Wall" unless $CPPFLAGS.split.include? "-Wall"
-$CPPFLAGS += " -g" unless $CPPFLAGS.split.include? "-g"
-$CPPFLAGS += " -rdynamic" unless $CPPFLAGS.split.include? "-rdynamic"
+%w{g O3 Wall rdynamic}.each do |flag|
+  flag = "-#{flag}"
+  $CPPFLAGS += " #{flag}" unless $CPPFLAGS.split.include? flag
+end
 
 CONFIG['LDSHARED'] = '$(CXX) -shared' unless RUBY_PLATFORM =~ /darwin/
 
