9e4a6b2c92c753367bb88148a755dad88331f2db
Vasiliy Ermolovich <younash@gmail.com>
2011-10-28 22:08:45 +0300

    bump version

--- lib/cityhash/version.rb
+++ lib/cityhash/version.rb
@@ -1,12 +1,12 @@
 module CityHash
- 
+
   module Version
     MAJOR = 0
-    MINOR = 4
+    MINOR = 5
     PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.')
   end
-  
+
 end
