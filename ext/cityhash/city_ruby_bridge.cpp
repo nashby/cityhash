186c37cc5acffa471db39cb0d0daf904c9ce9489
Vasiliy Ermolovich <younash@gmail.com>
2012-01-28 22:04:09 +0300

    add crc support

--- ext/cityhash/city_ruby_bridge.cpp
+++ ext/cityhash/city_ruby_bridge.cpp
@@ -17,3 +17,29 @@ const char* CityHash128WithSeedString(const char *s, size_t len, uint64 seed1, u
 
   return str.str().c_str();
 }
+
+const char* CityHashCrc128String(const char *s, size_t len)
+{
+  #ifdef __SSE4_2__
+  std::ostringstream str;
+  str << Uint128Low64(CityHashCrc128(s, len)) << Uint128High64(CityHashCrc128(s, len));
+
+  return str.str().c_str();
+  #else
+  return CityHash128String(s, len);
+  #endif
+}
+
+const char* CityHashCrc128WithSeedString(const char *s, size_t len, uint64 seed1, uint64 seed2)
+{
+  #ifdef __SSE4_2__
+  uint128 seed(seed1, seed2);
+  std::ostringstream str;
+  str << Uint128Low64(CityHashCrc128WithSeed(s, len, seed)) << Uint128High64(CityHashCrc128WithSeed(s, len, seed));
+
+  return str.str().c_str();
+  #else
+  return CityHash128WithSeedString(s, len, seed1, seed2);
+  #endif
+}
+
