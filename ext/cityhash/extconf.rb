090a8cd23bb3ea1e4a771b77616adbb982a999fc
Vasiliy Ermolovich <younash@gmail.com>
2013-05-18 00:04:34 +0300

    add ability to specify c++ compiler

--- ext/cityhash/extconf.rb
+++ ext/cityhash/extconf.rb
@@ -1,5 +1,7 @@
 require 'mkmf'
 
+RbConfig::MAKEFILE_CONFIG['CXX'] = ENV['CXX'] if ENV['CXX']
+
 %w{g O3 Wall march=native}.each do |flag|
   flag = "-#{flag}"
   $CPPFLAGS += " #{flag}" unless $CPPFLAGS.split.include? flag
