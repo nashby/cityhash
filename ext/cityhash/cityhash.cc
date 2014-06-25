e1678f50bd1a632fa4aab96aec2e5a9c369ed73b
Scott Francis <scott.francis@shopify.com>
2014-06-25 19:14:13 +0000

    Validate hash arguments

--- ext/cityhash/cityhash.cc
+++ ext/cityhash/cityhash.cc
@@ -16,27 +16,32 @@ extern "C" VALUE cityhash_hash32(VALUE mod, VALUE input)
 
 extern "C" VALUE cityhash_hash64(VALUE mod, VALUE input)
 {
+  Check_Type(input, T_STRING);
   return ULL2NUM(CityHash64(StringValuePtr(input), RSTRING_LEN(input)));
 }
 
 extern "C" VALUE cityhash_hash64_with_seed(VALUE mod, VALUE input, VALUE seed)
 {
+  Check_Type(input, T_STRING);
   return ULL2NUM(CityHash64WithSeed(StringValuePtr(input), RSTRING_LEN(input), NUM2ULL(seed)));
 }
 
 extern "C" VALUE cityhash_hash64_with_seeds(VALUE mod, VALUE input, VALUE seed1, VALUE seed2)
 {
+  Check_Type(input, T_STRING);
   return ULL2NUM(CityHash64WithSeeds(StringValuePtr(input), RSTRING_LEN(input), NUM2ULL(seed1), NUM2ULL(seed2)));
 }
 
 extern "C" VALUE cityhash_hash128(VALUE mod, VALUE input)
 {
+  Check_Type(input, T_STRING);
   uint128 hash = CityHash128(StringValuePtr(input), RSTRING_LEN(input));
   return rb_str_new((char *)&hash, sizeof(hash));
 }
 
 extern "C" VALUE cityhash_hash128_with_seed(VALUE mod, VALUE input, VALUE seed_string)
 {
+  Check_Type(input, T_STRING);
   uint128 seed = *(uint128 *)StringValuePtr(seed_string);
   uint128 hash = CityHash128WithSeed(StringValuePtr(input), RSTRING_LEN(input), seed);
   return rb_str_new((char *)&hash, sizeof(hash));
